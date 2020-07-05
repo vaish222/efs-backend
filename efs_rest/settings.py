98611a5722bf31c66b221cbf9e0968c6c011409c
Vaishali <vgoel@unomaha.edu>
2020-07-05 09:12:49 -0500

    changes for heroku

--- efs_rest/settings.py
+++ efs_rest/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'o_21*)hh*)8&rac*_h1o8z&--fuuj#m6#9+*vkyaiyp77k_lq6'
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = 'o_21*)hh*)8&rac*_h1o8z&--fuuj#m6#9+*vkyaiyp77k_lq6'
+#SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
